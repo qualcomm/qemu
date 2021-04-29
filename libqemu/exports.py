3e3acd1a393e7b5325f274827dcf29991a23442d
Luc Michel <luc.michel@greensocs.com>
2021-04-29 12:26:09 +0200

    libqemu: add exports for cpu_restore_state, cpu_loop_exit_noexc, cpu_get_mem_io_pc
    Those exports allow an I/O operation to be exited from the I/O callback
    itself.

--- libqemu/exports.py
+++ libqemu/exports.py
@@ -158,6 +158,9 @@ ExportedFct('current_cpu_get', 'Object *', [], priv = 'libqemu_current_cpu_get')
 ExportedFct('current_cpu_set', 'void', [ 'Object *' ], priv = 'libqemu_current_cpu_set')
 ExportedFct('async_safe_run_on_cpu', 'void', [ 'Object *', 'LibQemuAsyncCpuJobFn', 'void *' ],
         priv = 'libqemu_async_safe_run_on_cpu')
+ExportedFct('cpu_restore_state', 'bool', [ 'Object *', 'uintptr_t', 'bool'])
+ExportedFct('cpu_loop_exit_noexc', 'void', [ 'Object *' ])
+ExportedFct('cpu_get_mem_io_pc', 'uintptr_t', [ 'Object *' ], priv = 'libqemu_cpu_get_mem_io_pc')
 
 PrivateInclude('exec/gdbstub.h')
 ExportedFct('gdbserver_start', 'void', ['const char *'], on_iothread = True)
