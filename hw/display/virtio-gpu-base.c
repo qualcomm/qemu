10ea66462922fe1d0106af7d29b158861271989e
Antonio Caggiano <antonio.caggiano@collabora.com>
2021-07-16 14:41:50 +0200

    virtio-gpu: Resource UUID
    Enable resource UUID feature and implement command resource assign UUID.
    This is done by introducing a hash table to map resource IDs to their
    UUIDs.
    
    Signed-off-by: Antonio Caggiano <antonio.caggiano@collabora.com>
    Signed-off-by: Antonio Caggiano <quic_acaggian@quicinc.com>

--- hw/display/virtio-gpu-base.c
+++ hw/display/virtio-gpu-base.c
@@ -265,6 +265,8 @@ virtio_gpu_base_get_features(VirtIODevice *vdev, uint64_t features,
         features |= (1 << VIRTIO_GPU_F_RESOURCE_UUID);
     }
 
+    features |= (1 << VIRTIO_GPU_F_RESOURCE_UUID);
+
     return features;
 }
 
