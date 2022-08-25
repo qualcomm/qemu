aecd23caca1bfd872b5d98079f8c73c67d0a293f
Antonio Caggiano <antonio.caggiano@collabora.com>
2022-08-25 17:18:59 +0200

    virtio-gpu: CONTEXT_INIT feature
    Create virgl renderer context with flags using context_id when valid.
    The feature can be enabled via the context_init config option.
    A warning message will be emitted and the feature will not be used
    when linking with virglrenderer versions without context_init support.
    
    Signed-off-by: Antonio Caggiano <antonio.caggiano@collabora.com>
    Signed-off-by: Antonio Caggiano <quic_acaggian@quicinc.com>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- hw/display/virtio-gpu.c
+++ hw/display/virtio-gpu.c
@@ -1717,6 +1717,8 @@ static Property virtio_gpu_properties[] = {
                     VIRTIO_GPU_FLAG_BLOB_ENABLED, false),
     DEFINE_PROP_SIZE("hostmem", VirtIOGPU, parent_obj.conf.hostmem, 0),
     DEFINE_PROP_UINT8("x-scanout-vmstate-version", VirtIOGPU, scanout_vmstate_version, 2),
+    DEFINE_PROP_BIT("context_init", VirtIOGPU, parent_obj.conf.flags,
+                    VIRTIO_GPU_FLAG_CONTEXT_INIT_ENABLED, false),
     DEFINE_PROP_END_OF_LIST(),
 };
 
