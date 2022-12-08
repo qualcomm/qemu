d8f514dc8cf88f3dd814cdfbc01b57df19d69ec8
Brian Cain <bcain@quicinc.com>
2022-12-08 08:32:33 -0800

    Fix lld arg, set CC explicitly

--- qemu.cmake
+++ qemu.cmake
@@ -57,12 +57,13 @@ if (APPLE)
     --disable-opengl)
 endif()
 
+set(QEMU_CONF_ARGS ${QEMU_CONF_ARGS} --cc=${CMAKE_C_COMPILER})
 if (GS_ENABLE_SANITIZERS)
     set(QEMU_CONF_ARGS ${QEMU_CONF_ARGS} --enable-sanitizers)
 endif()
 
 if (GS_ENABLE_LLD)
-    set(QEMU_CONF_ARGS ${QEMU_CONF_ARGS} "--extra-ldflags='-fuse-ld=lld'")
+    set(QEMU_CONF_ARGS ${QEMU_CONF_ARGS} --extra-ldflags=-fuse-ld=lld)
 endif()
 
 if (GS_ENABLE_LTO)
