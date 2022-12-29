fefb9bf19f5ad63f6effdef9f7977e5ffa24f306
Matheus Tavares Bernardino <quic_mathbern@quicinc.com>
2022-12-29 15:52:11 -0300

    ARM: add a76-specific system regs
    This is the minimum set of registers to avoid an infinite loop of the
    following during QNX boot in qqvp (or at least seems to be):
    
    [W] [      1.508513001 s ]platform.fallback_memory      : b_transport :READ to address: 0x10000018 len: 4 data: 0x00000000 status: TLM_INCOMPLETE_RESPONSE  extn:2 extn:10
    [W] [      1.508513001 s ]platform.fallback_memory      : b_transport :WRITE to address: 0x10000000 len: 4 data: 0x00000030 status: TLM_INCOMPLETE_RESPONSE  extn:2 extn:10
    
    Signed-off-by: Matheus Tavares Bernardino <quic_mathbern@quicinc.com>

--- target/arm/internals.h
+++ target/arm/internals.h
@@ -1352,8 +1352,10 @@ void arm_cpu_lpa2_finalize(ARMCPU *cpu, Error **errp);
 
 #ifdef CONFIG_USER_ONLY
 static inline void define_cortex_a72_a57_a53_cp_reginfo(ARMCPU *cpu) { }
+static inline void define_cortex_a76_cp_reginfo(ARMCPU *cpu) { }
 #else
 void define_cortex_a72_a57_a53_cp_reginfo(ARMCPU *cpu);
+void define_cortex_a76_cp_reginfo(ARMCPU *cpu);
 #endif
 
 bool el_is_in_host(CPUARMState *env, int el);
