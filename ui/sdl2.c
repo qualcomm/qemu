7423515c76db611fbcb3b7b48185349655334ffe
Antonio Caggiano <quic_acaggian@quicinc.com>
2023-01-13 12:34:29 +0100

    SDL: OpenGL 3.0 window context
    SDL renderer creates an OpenGL 2.1 context while QEMU expects minimum
    version 3.0. To fix this we create an OpenGL 3.0 context directly,
    ignoring the SDL renderer when OpenGL is enabled.
    
    Signed-off-by: Antonio Caggiano <quic_acaggian@quicinc.com>

--- ui/sdl2.c
+++ ui/sdl2.c
@@ -91,17 +91,23 @@ void sdl2_window_create(struct sdl2_console *scon)
 #ifdef CONFIG_OPENGL
     if (scon->opengl) {
         flags |= SDL_WINDOW_OPENGL;
+        if (scon->opts->gl == DISPLAYGL_MODE_ES) {
+            SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+                                SDL_GL_CONTEXT_PROFILE_ES);
+        }
+        // This is the minimum version required by the texture blit shaders
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
     }
 #endif
 
-    scon->real_window = SDL_CreateWindow("", SDL_WINDOWPOS_UNDEFINED,
-                                         SDL_WINDOWPOS_UNDEFINED,
-                                         surface_width(scon->surface),
-                                         surface_height(scon->surface),
-                                         flags);
-    scon->real_renderer = SDL_CreateRenderer(scon->real_window, -1, 0);
+    scon->real_window = SDL_CreateWindow(
+        "", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        surface_width(scon->surface), surface_height(scon->surface), flags);
     if (scon->opengl) {
-        scon->winctx = SDL_GL_GetCurrentContext();
+        scon->winctx = SDL_GL_CreateContext(scon->real_window);
+    } else {
+        scon->real_renderer = SDL_CreateRenderer(scon->real_window, -1, 0);
     }
     sdl_update_caption(scon);
 }
@@ -112,8 +118,14 @@ void sdl2_window_destroy(struct sdl2_console *scon)
         return;
     }
 
-    SDL_DestroyRenderer(scon->real_renderer);
-    scon->real_renderer = NULL;
+    if (scon->winctx) {
+        SDL_GL_DeleteContext(scon->winctx);
+        scon->winctx = NULL;
+    }
+    if (scon->real_renderer) {
+        SDL_DestroyRenderer(scon->real_renderer);
+        scon->real_renderer = NULL;
+    }
     SDL_DestroyWindow(scon->real_window);
     scon->real_window = NULL;
 }
