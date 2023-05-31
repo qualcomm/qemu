8fe1247978411e7a387b004d33dd8eb6ae80ad2c
Antonio Caggiano <quic_acaggian@quicinc.com>
2023-05-31 12:32:48 +0200

    libqemu: Export address_space_update_topology
    Signed-off-by: Antonio Caggiano <quic_acaggian@quicinc.com>

--- include/exec/memory.h
+++ include/exec/memory.h
@@ -2638,6 +2638,8 @@ MemTxResult memory_region_dispatch_write(MemoryRegion *mr,
  */
 void address_space_init(AddressSpace *as, MemoryRegion *root, const char *name);
 
+void address_space_update_topology(AddressSpace *as);
+
 /**
  * address_space_destroy: destroy an address space
  *
