2075a8282e50221a0510ad5eb3ece0446af4aa11
Antonio Caggiano <quic_acaggian@quicinc.com>
2023-07-04 12:22:50 +0200

    libqemu: Export SDL2 create/init functions
    SDL2 consoles should be created within the QEMU's sdl2.c as input
    functions rely on a static variable holding them all. Implement and
    export some functions for creating and initializing SDL2 consoles
    reusing that static variable.
    
    Signed-off-by: Antonio Caggiano <quic_acaggian@quicinc.com>

--- ui/sdl2.c
+++ ui/sdl2.c
@@ -792,7 +792,7 @@ void sdl_mouse_define(DisplayChangeListener *dcl, QEMUCursor *c)
     }
 }
 
-static void sdl_cleanup(void)
+void sdl_cleanup(void)
 {
     if (guest_sprite) {
         SDL_FreeCursor(guest_sprite);
@@ -882,6 +882,19 @@ static void sdl2_set_hint_x11_force_egl(void)
 
     XCloseDisplay(x_disp);
 #endif
+}
+
+void sdl2_create_consoles(int num)
+{
+    assert(num > 0);
+    sdl2_num_outputs = num;
+    sdl2_console = g_new0(struct sdl2_console, sdl2_num_outputs);
+}
+
+struct sdl2_console *sdl2_get_console(int index)
+{
+    assert(index >= 0 && index < sdl2_num_outputs);
+    return &sdl2_console[index];
 }
 
 static void sdl2_display_init(DisplayState *ds, DisplayOptions *o)
