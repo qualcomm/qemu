2bedf8e1f772efc732c1db484a8c69cf071da3b7
Brian Cain <bcain@quicinc.com>
2023-08-25 09:01:46 -0300

    Hexagon (hex_mmu): add reset and extract realize
    Signed-off-by: Brian Cain <bcain@quicinc.com>
    Signed-off-by: Matheus Tavares Bernardino <quic_mathbern@quicinc.com>

--- target/hexagon/cpu.c
+++ target/hexagon/cpu.c
@@ -518,6 +518,7 @@ static void hexagon_cpu_reset_hold(Object *obj)
     ATOMIC_STORE(env->tlb_lock_state, HEX_LOCK_UNLOCKED);
     ATOMIC_STORE(env->ss_pending, false);
 
+    hex_mmu_reset(env);
     hexagon_cpu_soft_reset(env);
 #endif
 }
@@ -629,12 +630,12 @@ static void hexagon_cpu_realize(DeviceState *dev, Error **errp)
 
 #define HEXAGON_CFG_ADDR_BASE(addr) ((addr >> 16) & 0x0fffff)
 #ifndef CONFIG_USER_ONLY
+    hex_mmu_realize(env);
     if (cs->cpu_index == 0) {
         env->g_sreg = g_malloc0(sizeof(target_ulong) * NUM_SREGS);
         env->g_gcycle = g_malloc0(sizeof(target_ulong) * NUM_GLOBAL_GCYCLE);
         env->processor_ptr->shared_extptr = hmx_ext_palloc(env->processor_ptr, 0);
         hmx_configure_state(env);
-        hex_mmu_init(env);
         ARCH_SET_SYSTEM_REG(env, HEX_SREG_EVB, 0x0);
         ARCH_SET_SYSTEM_REG(env, HEX_SREG_LIVELOCK, 0x0);
         ARCH_SET_SYSTEM_REG(env, HEX_SREG_CFGBASE,
@@ -683,7 +684,6 @@ static void hexagon_cpu_realize(DeviceState *dev, Error **errp)
         }
         env->g_sreg = env0->g_sreg;
         env->g_gcycle = env0->g_gcycle;
-        env->hex_tlb = env0->hex_tlb;
         env->cmdline = env0->cmdline;
         env->lib_search_dir = env0->lib_search_dir;
         env->processor_ptr->shared_extptr = env0->processor_ptr->shared_extptr;
