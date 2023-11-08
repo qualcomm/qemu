c1c7522d990ee217636dcf39968bf5d7b3622cc5
Sid Manning <sidneym@quicinc.com>
2023-11-07 17:01:28 -0800

    hw/timer: Add QTimer device
    Note: QTimer was implemented before ARM SSE Timer was upstreamed, there may
    be opportunity to use that device instead.
    
    Co-authored-by: Damien Hedde <damien.hedde@dahe.fr>
    Co-authored-by: Tobias Röhmel <quic_trohmel@quicinc.com>
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- hw/hexagon/hexagon_dsp.c
+++ hw/hexagon/hexagon_dsp.c
@@ -3,7 +3,6 @@
  * subsystem with few peripherals, like the Compute DSP.
  *
  * Copyright (c) 2020-2024 Qualcomm Innovation Center, Inc. All Rights Reserved.
- *
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
@@ -120,6 +119,7 @@ static void hexagon_common_init(MachineState *machine, Rev_t rev,
          */
         qdev_prop_set_bit(DEVICE(cpu), "start-powered-off", (i != 0));
         qdev_prop_set_uint32(DEVICE(cpu), "l2vic-base-addr", m_cfg->l2vic_base);
+        qdev_prop_set_uint32(DEVICE(cpu), "qtimer-base-addr", m_cfg->qtmr_rg0);
         qdev_prop_set_uint32(DEVICE(cpu), "config-table-addr", m_cfg->cfgbase);
 
         if (i == 0) {
