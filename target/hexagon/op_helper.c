80dfee6fd4e2ef682b0c20451b464bb541ab21c8
Sid Manning <sidneym@quicinc.com>
2024-01-29 13:26:13 -0800

    [PATCH] Add new asserts, k0lock log tracing, queue the waiting lock
    To enable new log traces:
    
    From the command line: -d trace:hexagon_k0_lock
    From the monitor: log trace:hexagon_k0_lock
    
    From the command line: -d trace:hexagon_k0_lock_info
    From the monitor: log trace:hexagon_k0_lock_info
    
    trace:hexagon_k0_lock_info supplies more context to the sequence of
    locking and unlocking without having to define HEX_DEBUG
    
    With this change 2 back to back k0locks will hang the thread which
    is the expected behavior.  k0locklock test added.
    
    Discard volatile variable designation of k0lock.
    
    Signed-off-by: Sid Manning <sidneym@quicinc.com>

--- target/hexagon/op_helper.c
+++ target/hexagon/op_helper.c
@@ -50,6 +50,7 @@
 #include "pmu.h"
 #endif
 #include "mmvec/macros.h"
+#include "trace.h"
 
 #define SF_BIAS        127
 #define SF_MANTBITS    23
@@ -1736,29 +1737,46 @@ static void hex_k0_lock(CPUHexagonState *env)
 {
     const bool exception_context = qemu_mutex_iothread_locked();
     LOCK_IOTHREAD(exception_context);
-    HEX_DEBUG_LOG("Before hex_k0_lock: %d\n", env->threadId);
+    trace_hexagon_k0_lock_info(env->threadId, "Before hex_k0_lock");
     print_thread_states("\tThread");
+    trace_hexagon_k0_lock(env->threadId, env->next_PC, env->k0_lock_count);
+    g_assert((env->k0_lock_count == 0) || (env->k0_lock_count == 1));
 
     uint32_t syscfg = ARCH_GET_SYSTEM_REG(env, HEX_SREG_SYSCFG);
     if (GET_SYSCFG_FIELD(SYSCFG_K0LOCK, syscfg)) {
-        if (ATOMIC_LOAD(env->k0_lock_state) == HEX_LOCK_OWNER) {
+        if (ATOMIC_LOAD(env->k0_lock_state) == HEX_LOCK_QUEUED) {
             env->next_PC += 4;
-            HEX_DEBUG_LOG("Already the owner\n");
+            env->k0_lock_count++;
+            ATOMIC_STORE(env->k0_lock_state, HEX_LOCK_OWNER);
+            SET_SYSCFG_FIELD(env, SYSCFG_K0LOCK, 1);
+            trace_hexagon_k0_lock_info(env->threadId,
+                                       "queued thread waiting gets lock\n");
             UNLOCK_IOTHREAD(exception_context);
             return;
         }
-        HEX_DEBUG_LOG("\tWaiting\n");
+        if (ATOMIC_LOAD(env->k0_lock_state) == HEX_LOCK_OWNER) {
+            qemu_log_mask(LOG_GUEST_ERROR,
+                          "Double k0lock at PC: 0x%x, thread may hang\n",
+                          env->next_PC);
+            env->next_PC += 4;
+            CPUState *cs = env_cpu(env);
+            cpu_interrupt(cs, CPU_INTERRUPT_HALT);
+            UNLOCK_IOTHREAD(exception_context);
+            return;
+        }
+        trace_hexagon_k0_lock_info(env->threadId, "Waiting for k0lock");
         ATOMIC_STORE(env->k0_lock_state, HEX_LOCK_WAITING);
         CPUState *cs = env_cpu(env);
         cpu_interrupt(cs, CPU_INTERRUPT_HALT);
     } else {
-        HEX_DEBUG_LOG("\tAcquired\n");
+        trace_hexagon_k0_lock_info(env->threadId, "Acquired k0lock");
         env->next_PC += 4;
+        env->k0_lock_count++;
         ATOMIC_STORE(env->k0_lock_state, HEX_LOCK_OWNER);
         SET_SYSCFG_FIELD(env, SYSCFG_K0LOCK, 1);
     }
 
-    HEX_DEBUG_LOG("After hex_k0_lock: %d\n", env->threadId);
+    trace_hexagon_k0_lock_info(env->threadId, "After hex_k0_lock\n");
     print_thread_states("\tThread");
     UNLOCK_IOTHREAD(exception_context);
 }
@@ -1766,8 +1784,10 @@ static void hex_k0_lock(CPUHexagonState *env)
 static void hex_k0_unlock(CPUHexagonState *env)
 {
     qemu_mutex_lock_iothread();
-    HEX_DEBUG_LOG("Before hex_k0_unlock: %d\n", env->threadId);
+    trace_hexagon_k0_lock_info(env->threadId, "Before hex_k0_unlock");
     print_thread_states("\tThread");
+    trace_hexagon_k0_lock(env->threadId, env->next_PC, env->k0_lock_count);
+    g_assert((env->k0_lock_count == 0) || (env->k0_lock_count == 1));
 
     /* Nothing to do if the k0 isn't locked by this thread */
     uint32_t syscfg = ARCH_GET_SYSTEM_REG(env, HEX_SREG_SYSCFG);
@@ -1781,6 +1801,8 @@ static void hex_k0_unlock(CPUHexagonState *env)
         return;
     }
 
+    trace_hexagon_k0_lock_info(env->threadId, "Unlocking k0lock");
+    env->k0_lock_count--;
     ATOMIC_STORE(env->k0_lock_state, HEX_LOCK_UNLOCKED);
     SET_SYSCFG_FIELD(env, SYSCFG_K0LOCK, 0);
 
@@ -1827,12 +1849,13 @@ static void hex_k0_unlock(CPUHexagonState *env)
         cs = env_cpu(unlock_thread);
 
         print_thread("\tWaiting thread found", cs);
-        ATOMIC_STORE(unlock_thread->k0_lock_state, HEX_LOCK_OWNER);
+        trace_hexagon_k0_lock_info(unlock_thread->threadId, "Will get the next k0lock");
+        ATOMIC_STORE(unlock_thread->k0_lock_state, HEX_LOCK_QUEUED);
         SET_SYSCFG_FIELD(unlock_thread, SYSCFG_K0LOCK, 1);
         cpu_interrupt(cs, CPU_INTERRUPT_K0_UNLOCK);
     }
 
-    HEX_DEBUG_LOG("After hex_k0_unlock: %d\n", env->threadId);
+    trace_hexagon_k0_lock_info(env->threadId, "After hex_k0_unlock\n");
     print_thread_states("\tThread");
     qemu_mutex_unlock_iothread();
 }
