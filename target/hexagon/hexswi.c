d10221a4983f5886670667f871aaf8b7b472024f
Sid Manning <sidneym@quicinc.com>
2024-02-08 15:31:57 -0300

    hexswi: remove unnecessary rcu_lock
    Signed-off-by: Matheus Tavares Bernardino <quic_mathbern@quicinc.com>

--- target/hexagon/hexswi.c
+++ target/hexagon/hexswi.c
@@ -190,11 +190,9 @@ static int sim_handle_trap_functional(CPUHexagonState *env)
     {
         FILE *fp = stdout;
         char c;
-        rcu_read_lock();
         DEBUG_MEMORY_READ(swi_info, 1, &c);
         fprintf(fp, "%c", c);
         fflush(fp);
-        rcu_read_unlock();
     }
     break;
 
@@ -213,14 +211,12 @@ static int sim_handle_trap_functional(CPUHexagonState *env)
         FILE *fp = stdout;
         char c;
         int i = 0;
-        rcu_read_lock();
         do {
             DEBUG_MEMORY_READ(swi_info + i, 1, &c);
             fprintf(fp, "%c", c);
             i++;
         } while (c);
         fflush(fp);
-        rcu_read_unlock();
         break;
     }
 
@@ -248,7 +244,6 @@ static int sim_handle_trap_functional(CPUHexagonState *env)
                 __func__, count);
         }
 
-        rcu_read_lock();
         for (int i = 0; i < count; i++) {
             DEBUG_MEMORY_READ(bufaddr + i, 1, &buf[i]);
         }
@@ -264,7 +259,6 @@ static int sim_handle_trap_functional(CPUHexagonState *env)
         } else {
             ARCH_SET_THREAD_REG(env, HEX_REG_R00, count - retval);
         }
-        rcu_read_unlock();
         free(buf);
     }
     break;
