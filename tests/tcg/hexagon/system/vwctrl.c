bec2addddd4c9935551b4a26dc9850842bd12845
Matheus Tavares Bernardino <quic_mathbern@quicinc.com>
2024-02-09 11:29:55 -0300

    Hexagon: add test for new VWCTRL register
    Signed-off-by: Matheus Tavares Bernardino <quic_mathbern@quicinc.com>

---
+++ tests/tcg/hexagon/system/vwctrl.c
@@ -0,0 +1,179 @@
+/*
+ *  Copyright(c) 2024 Qualcomm Innovation Center, Inc. All Rights Reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <stdbool.h>
+#include <hexagon_types.h>
+#include <hexagon_protos.h>
+#include <hexagon_standalone.h>
+#include "filename.h"
+#include "cfgtable.h"
+#include "vtcm_common.h"
+#include "mmu.h"
+
+int err;
+bool window_miss_seen;
+#define HEX_CAUSE_VWCTRL_WINDOW_MISS 0x29
+#define HEX_CAUSE_PRIV_USER_NO_SINS 0x1b
+#define HEX_CAUSE_PRIV_USER_NO_GINS 0x1a
+
+void my_err_handler_helper(uint32_t ssr)
+{
+    uint32_t cause = GET_FIELD(ssr, SSR_CAUSE);
+    switch (cause) {
+    case HEX_CAUSE_VWCTRL_WINDOW_MISS:
+        window_miss_seen = true;
+        inc_elr(4);
+        break;
+    case HEX_CAUSE_PRIV_USER_NO_SINS:
+    case HEX_CAUSE_PRIV_USER_NO_GINS:
+        enter_kernel_mode();
+        break;
+    default:
+        do_coredump();
+        break;
+    }
+}
+
+MY_EVENT_HANDLE(my_event_handle_error, my_err_handler_helper)
+
+DEFAULT_EVENT_HANDLE(my_event_handle_nmi,         HANDLE_NMI_OFFSET)
+DEFAULT_EVENT_HANDLE(my_event_handle_tlbmissrw,   HANDLE_TLBMISSRW_OFFSET)
+DEFAULT_EVENT_HANDLE(my_event_handle_tlbmissx,    HANDLE_TLBMISSX_OFFSET)
+DEFAULT_EVENT_HANDLE(my_event_handle_reset,       HANDLE_RESET_OFFSET)
+DEFAULT_EVENT_HANDLE(my_event_handle_rsvd,        HANDLE_RSVD_OFFSET)
+DEFAULT_EVENT_HANDLE(my_event_handle_trap0,       HANDLE_TRAP0_OFFSET)
+DEFAULT_EVENT_HANDLE(my_event_handle_trap1,       HANDLE_TRAP1_OFFSET)
+DEFAULT_EVENT_HANDLE(my_event_handle_int,         HANDLE_INT_OFFSET)
+
+#define BUFFER_SIZE 64
+void test_scatter(uintptr_t scatter_addr)
+{
+    const unsigned int region_len = 1;
+    unsigned short offsets[BUFFER_SIZE];
+    unsigned short values[BUFFER_SIZE];
+    Q6_vscatter_RMVhV(scatter_addr, region_len,
+                      *(HVX_Vector *)offsets, *(HVX_Vector *)values);
+}
+
+bool is_vwctrl_enabled(void)
+{
+    uint32_t vwctrl;
+    asm volatile("%0 = vwctrl\n" : "=r"(vwctrl));
+    return vwctrl >> 31;
+}
+
+void get_vwctrl(bool *enable, uint32_t *vwctrl_lo, uint32_t *vwctrl_hi)
+{
+    uint32_t vwctrl;
+    asm volatile("%0 = vwctrl\n" : "=r"(vwctrl));
+    *enable = (vwctrl >> 31);
+    *vwctrl_lo = vwctrl & 0xfff;
+    *vwctrl_hi = (vwctrl >> 16) & 0xfff;
+}
+
+void set_vwctrl(bool enable, uint32_t vwctrl_lo, uint32_t vwctrl_hi)
+{
+    uint32_t vwctrl = (vwctrl_lo & 0xfff) |
+                      ((vwctrl_hi & 0xfff) << 16) | (enable << 31);
+    asm volatile("vwctrl = %0\n" : : "r"(vwctrl));
+}
+
+void test_vtcm_access(uint32_t access_addr, uint32_t wmin, uint32_t wmax,
+                      bool expect_failure)
+{
+    printf("Trying to access 0x%08"PRIx32"; vwctrl is %s; "
+           "VTCM window is [0x%08"PRIx32", 0x%08"PRIx32"]\n",
+           access_addr, is_vwctrl_enabled() ? "enabled" : "DISABLED", wmin, wmax);
+    printf("    expect %s: ", expect_failure ? "failure" : "success");
+
+    enter_user_mode();
+    test_scatter(access_addr);
+    check32(window_miss_seen, expect_failure);
+    printf("ok\n");
+}
+
+void test_coproc_vtcm_access(uint32_t access_addr, uint32_t wmin, uint32_t wmax,
+                             bool expect_failure)
+{
+    printf("[Coproc] Trying to access 0x%08"PRIx32"; vwctrl is %s; "
+           "VTCM window is [0x%08"PRIx32", 0x%08"PRIx32"]\n",
+           access_addr, is_vwctrl_enabled() ? "enabled" : "DISABLED", wmin, wmax);
+    printf("    expect %s: ", expect_failure ? "failure" : "success");
+
+    enter_user_mode();
+    Q6_mxmem2_bias_A((void *)access_addr);
+    check32(window_miss_seen, expect_failure);
+    printf("ok\n");
+}
+
+void calc_win_boundaries(uint32_t vtcm_base_addr, uint32_t vwctrl_lo,
+                         uint32_t vwctrl_hi, uint32_t *wmin, uint32_t *wmax)
+{
+    /*
+     * As per v79 sys spec:
+     * Upper legal address is calculated as vtcm_base_addr + ((HI+1)*4KB)-1
+     * Lower legal address is calculated as vtcm_base_addr + (LOW*4KB)
+     */
+    *wmin = vtcm_base_addr + (vwctrl_lo * 4 * 1024);
+    *wmax = vtcm_base_addr + ((vwctrl_hi + 1) * 4 * 1024) - 1;
+}
+
+int main()
+{
+    uint32_t vwctrl_lo, vwctrl_hi, wmin, wmax, vtcm_base_addr;
+    bool enable;
+
+    install_my_event_vectors();
+    setup_default_vtcm();
+    vtcm_base_addr = get_vtcm_base();
+    printf("VTCM base is 0x%08"PRIx32"\n", vtcm_base_addr);
+
+    /* acquire coproc */
+    asm volatile("R6=SSR\n"
+                 "R6=setbit(R6, #26)\n"
+                 "SSR = R6\n"
+                 "{ nop; }\n"
+                 "{ nop; }\n"
+                 "isync;\n"
+                 :
+                 :
+                 : "r6");
+
+    get_vwctrl(&enable, &vwctrl_lo, &vwctrl_hi);
+    calc_win_boundaries(vtcm_base_addr, vwctrl_lo, vwctrl_hi, &wmin, &wmax);
+
+    /* normal access */
+    test_vtcm_access(wmin, wmin, wmax, false);
+    test_coproc_vtcm_access(wmin, wmin, wmax, false);
+
+    /* vwctrl disabled */
+    set_vwctrl(false, vwctrl_lo, vwctrl_hi);
+    test_vtcm_access(wmin, wmin, wmax, true);
+    test_coproc_vtcm_access(wmin, wmin, wmax, true);
+
+    /* out of bounds access */
+    calc_win_boundaries(vtcm_base_addr, vwctrl_lo + 1, vwctrl_hi, &wmin, &wmax);
+    set_vwctrl(true, vwctrl_lo + 1, vwctrl_hi);
+    test_vtcm_access(wmin, wmin, wmax, true);
+    test_coproc_vtcm_access(wmin, wmin, wmax, true);
+
+    printf("%s : %s\n", ((err) ? "FAIL" : "PASS"), __FILENAME__);
+    return err;
+}
