b8ec165ab015d30a8cb7be6ce40fa72662053ac4
Sid Manning <sidneym@quicinc.com>
2024-02-21 09:26:32 -0800

    Fix makefile error update testcase
    This line prevented many tests from running.
    
    Signed-off-by: Sid Manning <sidneym@quicinc.com>

--- tests/tcg/hexagon/system/pmu.c
+++ tests/tcg/hexagon/system/pmu.c
@@ -429,16 +429,18 @@ static void test_upmucnt(void)
     /* gpmucnt writes should be ignored. */
     toggle_ssr_pe(1);
     pmu_reset();
+
+    /* The compiler prevents this, so we write the word directly. */
     asm volatile(
         "r0 = #2\n"
-        "c20 = r0\n"
-        "c21 = r0\n"
-        "c22 = r0\n"
-        "c23 = r0\n"
-        "c24 = r0\n"
-        "c25 = r0\n"
-        "c26 = r0\n"
-        "c27 = r0\n"
+        ".word 0x6220c014\n" /* c20 = r0 */
+        ".word 0x6220c015\n" /* c21 = r0 */
+        ".word 0x6220c016\n" /* c22 = r0 */
+        ".word 0x6220c017\n" /* c23 = r0 */
+        ".word 0x6220c018\n" /* c24 = r0 */
+        ".word 0x6220c019\n" /* c25 = r0 */
+        ".word 0x6220c01a\n" /* c26 = r0 */
+        ".word 0x6220c01b\n" /* c27 = r0 */
         : : : "r0");
     for (int i = 0; i < NUM_PMU_CTRS; i++) {
         check_range(i, CREG, 0, 0);
