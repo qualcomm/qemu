ec46b6019d4df9510d9853a296c738591b9f1ecc
Brian Cain <bcain@quicinc.com>
2024-04-23 20:38:16 -0700

    use enumerate, code_fmt
    Signed-off-by: Brian Cain <bcain@quicinc.com>

--- target/hexagon/gen_helper_funcs.py
+++ target/hexagon/gen_helper_funcs.py
@@ -101,23 +101,28 @@ def gen_helper_function(f, tag, tagregs, tagimms):
         """))
 
     if "A_COPROC" in hex_common.attribdict[tag]:
-        f.write("    paddr_t __attribute__((unused)) phys = 0;\n")
-        f.write("    int __attribute__((unused)) prot;\n")
-        f.write("    int32_t __attribute__((unused)) excp;\n")
-        f.write("    CoprocArgs args = {0};\n")
-        f.write(f"    args.opcode = {tag};\n")
-        f.write(f"    args.unit = env->threadId;\n")
+        f.write(hex_common.code_fmt(f"""\
+    paddr_t __attribute__((unused)) phys = 0;
+    int __attribute__((unused)) prot;
+    int32_t __attribute__((unused)) excp;
+    CoprocArgs args = {0};
+    args.opcode = {tag};
+    args.unit = env->threadId;
+"""))
         for i, regtype, regid in enumerate(regs, 1):
             if (regtype == 'R' and regid == 's'):
-                f.write(f"    hex_tlb_find_match(env, {regtype}{regid}V,"
-                        + " MMU_DATA_LOAD, &phys, &prot, &args.page_size,"
-                        + " &excp, cpu_mmu_index(env_cpu(thread), false));\n")
-                f.write(f"    args.arg{i} = phys + ({regtype}{regid}V"
-                        + " & (args.page_size - 1));\n")
+                f.write(hex_common.code_fmt(f"""\
+    hex_tlb_find_match(env, {regtype}{regid}V, MMU_DATA_LOAD,
+        &phys, &prot, &args.page_size, &excp, cpu_mmu_index(env_cpu(thread),
+        false));
+    args.arg{i} = phys + ({regtype}{regid}V & (args.page_size - 1));
+"""))
             else:
                 f.write(f"    args.arg{i} = {regtype}{regid}V;\n")
-        f.write("    coproc(&args);\n")
-        f.write("}\n\n")
+        f.write("""    coproc(&args);
+}
+
+""")
         ## End of the helper definition
         return
 
