b9c2c517dca3c957ab53046f8a33389350fdd5eb
Brian Cain <bcain@quicinc.com>
2024-04-23 20:42:11 -0700

    move code to coproc
    Signed-off-by: Brian Cain <bcain@quicinc.com>

--- target/hexagon/gen_helper_funcs.py
+++ target/hexagon/gen_helper_funcs.py
@@ -22,6 +22,10 @@ import re
 import string
 import hex_common
 
+# QUIC specific
+import quic_coproc
+# end-QUIC specific
+
 def gen_decl_insn(f, slot):
     f.write(hex_common.code_fmt(f"""\
         Insn tmp_insn = {{ .slot = {slot} }};\n
@@ -100,31 +104,12 @@ def gen_helper_function(f, tag, tagregs, tagimms):
             uint32_t slot = slotval >> 1;
         """))
 
-    if "A_COPROC" in hex_common.attribdict[tag]:
-        f.write(hex_common.code_fmt(f"""\
-    paddr_t __attribute__((unused)) phys = 0;
-    int __attribute__((unused)) prot;
-    int32_t __attribute__((unused)) excp;
-    CoprocArgs args = {0};
-    args.opcode = {tag};
-    args.unit = env->threadId;
-"""))
-        for i, regtype, regid in enumerate(regs, 1):
-            if (regtype == 'R' and regid == 's'):
-                f.write(hex_common.code_fmt(f"""\
-    hex_tlb_find_match(env, {regtype}{regid}V, MMU_DATA_LOAD,
-        &phys, &prot, &args.page_size, &excp, cpu_mmu_index(env_cpu(thread),
-        false));
-    args.arg{i} = phys + ({regtype}{regid}V & (args.page_size - 1));
-"""))
-            else:
-                f.write(f"    args.arg{i} = {regtype}{regid}V;\n")
-        f.write("""    coproc(&args);
-}
-
-""")
+    # QUIC specific
+    if quic_coproc.handle_coproc(tag, regs, f):
+        f.write("}\n\n")
         ## End of the helper definition
         return
+    # end-QUIC specific
 
     if "A_FPOP" in hex_common.attribdict[tag]:
         f.write(hex_common.code_fmt(f"""\
