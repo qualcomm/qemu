aa276f645d67f3a606119a1a87feb5be54381e79
Brian Cain <bcain@quicinc.com>
2024-10-21 17:44:41 -0700

    Download the public build of the minivm tests
    Instead of the downstream build, use the public tarball for these
    tests.
    
    Also: increase the test timeout because this tarball takes a bit
    longer to decompress than the downstream one.
    
    Signed-off-by: Brian Cain <bcain@quicinc.com>

--- tests/functional/test_hexagon_minivm.py
+++ tests/functional/test_hexagon_minivm.py
@@ -13,22 +13,30 @@ from qemu_test.utils import archive_extract
 
 class MiniVMTest(QemuSystemTest):
 
+    timeout = 180
     GUEST_ENTRY = 0xc0000000
 
-    REPO = 'https://gitlab.qualcomm.com/qqvp/qemu/hexagonMVM'
+    REPO = 'https://artifacts.codelinaro.org/artifactory'
     ASSET_TARBALL = \
-        Asset(f'{REPO}/-/jobs/3664197/artifacts/raw/artifacts.tar.gz',
-              '27d7090744fd69e35bd6b891d484d8d48aa5277c1719be7c584b674b204f00ed')
+        Asset(f'{REPO}/codelinaro-toolchain-for-hexagon/19.1.2/'
+                'hexagon_rootfs_2024_Oct_22.tar.xz',
+        'a5c2bc8c1dddbe5ef4c375c84f0145ad61d116bb465669fc30690839720e6904')
 
     def test_minivm(self):
         tarball_path = self.ASSET_TARBALL.fetch()
-        archive_extract(tarball_path, self.workdir)
-        kernel_path = f'{self.workdir}/artifacts/minivm'
+        contents = ('boot/minivm', 'boot/test_mmu', 'boot/test_processors',
+            'boot/test_interrupts',
+            )
+        for f in contents:
+            f = os.path.join('hexagon-unknown-linux-musl-rootfs', f)
+            archive_extract(tarball_path, self.workdir, member=f)
+        rootfs_path = f'{self.workdir}/hexagon-unknown-linux-musl-rootfs'
+        kernel_path = f'{rootfs_path}/boot/minivm'
 
         self.set_machine('SA8775P_CDSP0')
 
-        for test_bin_path in glob(f'{self.workdir}/artifacts/tests_bin/*'):
-            print(f'# Testing {os.path.basename(test_bin_path)}')
+        for test_bin_path in glob(f'{rootfs_path}/boot/test_*'):
+            print(f'# Testing "{os.path.basename(test_bin_path)}"')
 
             vm = self.get_vm()
             vm.add_args('-kernel', kernel_path,
