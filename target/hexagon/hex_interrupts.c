e2c10a20a853c70a53a527d37f88f34dfd57655a
Brian Cain <brian.cain@oss.qualcomm.com>
2025-01-10 14:47:16 -0800

    target/hexagon: review feedback ints
    review feedback:
        * remove ARCH_{S,G}ET_* macros
        * Replace &cpu->env with cpu_env(cs)

--- target/hexagon/hex_interrupts.c
+++ target/hexagon/hex_interrupts.c
@@ -1,7 +1,18 @@
 /*
- * Copyright(c) 2022-2024 Qualcomm Innovation Center, Inc. All Rights Reserved.
+ *  Copyright(c) 2022 Qualcomm Innovation Center, Inc. All Rights Reserved.
  *
- * SPDX-License-Identifier: BSD-3-Clause
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "qemu/osdep.h"
@@ -12,115 +23,114 @@
 #include "macros.h"
 #include "sys_macros.h"
 #include "system/cpus.h"
-#include "cpu_helper.h"
 
 static bool hex_is_qualified_for_int(CPUHexagonState *env, int int_num);
 
 static bool get_syscfg_gie(CPUHexagonState *env)
 {
-    target_ulong syscfg = ARCH_GET_SYSTEM_REG(env, HEX_SREG_SYSCFG);
+    target_ulong syscfg = arch_get_system_reg(env, HEX_SREG_SYSCFG);
     return GET_SYSCFG_FIELD(SYSCFG_GIE, syscfg);
 }
 
 static bool get_ssr_ex(CPUHexagonState *env)
 {
-    target_ulong ssr = ARCH_GET_SYSTEM_REG(env, HEX_SREG_SSR);
+    target_ulong ssr = arch_get_system_reg(env, HEX_SREG_SSR);
     return GET_SSR_FIELD(SSR_EX, ssr);
 }
 
 static bool get_ssr_ie(CPUHexagonState *env)
 {
-    target_ulong ssr = ARCH_GET_SYSTEM_REG(env, HEX_SREG_SSR);
+    target_ulong ssr = arch_get_system_reg(env, HEX_SREG_SSR);
     return GET_SSR_FIELD(SSR_IE, ssr);
 }
 
 /* Do these together so we only have to call hexagon_modify_ssr once */
 static void set_ssr_ex_cause(CPUHexagonState *env, int ex, uint32_t cause)
 {
-    target_ulong old = ARCH_GET_SYSTEM_REG(env, HEX_SREG_SSR);
+    target_ulong old = arch_get_system_reg(env, HEX_SREG_SSR);
     SET_SYSTEM_FIELD(env, HEX_SREG_SSR, SSR_EX, ex);
     SET_SYSTEM_FIELD(env, HEX_SREG_SSR, SSR_CAUSE, cause);
-    target_ulong new = ARCH_GET_SYSTEM_REG(env, HEX_SREG_SSR);
+    target_ulong new = arch_get_system_reg(env, HEX_SREG_SSR);
     hexagon_modify_ssr(env, new, old);
 }
 
 static bool get_iad_bit(CPUHexagonState *env, int int_num)
 {
-    target_ulong ipendad = ARCH_GET_SYSTEM_REG(env, HEX_SREG_IPENDAD);
+    target_ulong ipendad = arch_get_system_reg(env, HEX_SREG_IPENDAD);
     target_ulong iad = GET_FIELD(IPENDAD_IAD, ipendad);
     return extract32(iad, int_num, 1);
 }
 
 static void set_iad_bit(CPUHexagonState *env, int int_num, int val)
 {
-    target_ulong ipendad = ARCH_GET_SYSTEM_REG(env, HEX_SREG_IPENDAD);
+    target_ulong ipendad = arch_get_system_reg(env, HEX_SREG_IPENDAD);
     target_ulong iad = GET_FIELD(IPENDAD_IAD, ipendad);
     iad = deposit32(iad, int_num, 1, val);
     fSET_FIELD(ipendad, IPENDAD_IAD, iad);
-    ARCH_SET_SYSTEM_REG(env, HEX_SREG_IPENDAD, ipendad);
+    arch_set_system_reg(env, HEX_SREG_IPENDAD, ipendad);
 }
 
 static uint32_t get_ipend(CPUHexagonState *env)
 {
-    target_ulong ipendad = ARCH_GET_SYSTEM_REG(env, HEX_SREG_IPENDAD);
+    target_ulong ipendad = arch_get_system_reg(env, HEX_SREG_IPENDAD);
     return GET_FIELD(IPENDAD_IPEND, ipendad);
 }
 
 static inline bool get_ipend_bit(CPUHexagonState *env, int int_num)
 {
-    target_ulong ipendad = ARCH_GET_SYSTEM_REG(env, HEX_SREG_IPENDAD);
+    target_ulong ipendad = arch_get_system_reg(env, HEX_SREG_IPENDAD);
     target_ulong ipend = GET_FIELD(IPENDAD_IPEND, ipendad);
     return extract32(ipend, int_num, 1);
 }
 
 static void clear_ipend(CPUHexagonState *env, uint32_t mask)
 {
-    target_ulong ipendad = ARCH_GET_SYSTEM_REG(env, HEX_SREG_IPENDAD);
+    target_ulong ipendad = arch_get_system_reg(env, HEX_SREG_IPENDAD);
     target_ulong ipend = GET_FIELD(IPENDAD_IPEND, ipendad);
     ipend &= ~mask;
     fSET_FIELD(ipendad, IPENDAD_IPEND, ipend);
-    ARCH_SET_SYSTEM_REG(env, HEX_SREG_IPENDAD, ipendad);
+    arch_set_system_reg(env, HEX_SREG_IPENDAD, ipendad);
 }
 
 static void set_ipend(CPUHexagonState *env, uint32_t mask)
 {
-    target_ulong ipendad = ARCH_GET_SYSTEM_REG(env, HEX_SREG_IPENDAD);
+    target_ulong ipendad = arch_get_system_reg(env, HEX_SREG_IPENDAD);
     target_ulong ipend = GET_FIELD(IPENDAD_IPEND, ipendad);
     ipend |= mask;
     fSET_FIELD(ipendad, IPENDAD_IPEND, ipend);
-    ARCH_SET_SYSTEM_REG(env, HEX_SREG_IPENDAD, ipendad);
+    arch_set_system_reg(env, HEX_SREG_IPENDAD, ipendad);
 }
 
 static void set_ipend_bit(CPUHexagonState *env, int int_num, int val)
 {
-    target_ulong ipendad = ARCH_GET_SYSTEM_REG(env, HEX_SREG_IPENDAD);
+    target_ulong ipendad = arch_get_system_reg(env, HEX_SREG_IPENDAD);
     target_ulong ipend = GET_FIELD(IPENDAD_IPEND, ipendad);
     ipend = deposit32(ipend, int_num, 1, val);
     fSET_FIELD(ipendad, IPENDAD_IPEND, ipend);
-    ARCH_SET_SYSTEM_REG(env, HEX_SREG_IPENDAD, ipendad);
+    arch_set_system_reg(env, HEX_SREG_IPENDAD, ipendad);
 }
 
 static bool get_imask_bit(CPUHexagonState *env, int int_num)
 {
-    target_ulong imask = ARCH_GET_SYSTEM_REG(env, HEX_SREG_IMASK);
+    target_ulong imask = arch_get_system_reg(env, HEX_SREG_IMASK);
     return extract32(imask, int_num, 1);
 }
 
 static uint32_t get_prio(CPUHexagonState *env)
 {
-    target_ulong stid = ARCH_GET_SYSTEM_REG(env, HEX_SREG_STID);
+    target_ulong stid = arch_get_system_reg(env, HEX_SREG_STID);
     return extract32(stid, reg_field_info[STID_PRIO].offset,
                      reg_field_info[STID_PRIO].width);
 }
 
 static void set_elr(CPUHexagonState *env, target_ulong val)
 {
-    ARCH_SET_SYSTEM_REG(env, HEX_SREG_ELR, val);
+    arch_set_system_reg(env, HEX_SREG_ELR, val);
 }
 
 static bool get_schedcfgen(CPUHexagonState *env)
 {
-    target_ulong schedcfg = ARCH_GET_SYSTEM_REG(env, HEX_SREG_SCHEDCFG);
+    target_ulong schedcfg = arch_get_system_reg(env, HEX_SREG_SCHEDCFG);
     return extract32(schedcfg, reg_field_info[SCHEDCFG_EN].offset,
                      reg_field_info[SCHEDCFG_EN].width);
 }
@@ -131,8 +141,7 @@ static bool is_lowest_prio(CPUHexagonState *env, int int_num)
     CPUState *cs;
 
     CPU_FOREACH(cs) {
-        HexagonCPU *hex_cpu = HEXAGON_CPU(cs);
-        CPUHexagonState *hex_env = &hex_cpu->env;
+        CPUHexagonState *hex_env = cpu_env(cs);
         if (!hex_is_qualified_for_int(hex_env, int_num)) {
             continue;
         }
@@ -184,7 +193,7 @@ static void restore_state(CPUHexagonState *env, bool int_accepted)
 static void hex_accept_int(CPUHexagonState *env, int int_num)
 {
     CPUState *cs = env_cpu(env);
-    target_ulong evb = ARCH_GET_SYSTEM_REG(env, HEX_SREG_EVB);
+    target_ulong evb = arch_get_system_reg(env, HEX_SREG_EVB);
     const int exe_mode = get_exe_mode(env);
     const bool in_wait_mode = exe_mode == HEX_EXE_MODE_WAIT;
 
@@ -218,7 +227,7 @@ bool hex_check_interrupts(CPUHexagonState *env)
     CPUState *cs = env_cpu(env);
     bool int_handled = false;
     bool ssr_ex = get_ssr_ex(env);
-    int max_ints;
+    int max_ints = 32;
     bool schedcfgen;
 
     /* Early exit if nothing pending */
@@ -227,7 +236,6 @@ bool hex_check_interrupts(CPUHexagonState *env)
         return false;
     }
 
-    max_ints = reg_field_info[IPENDAD_IPEND].width;
     BQL_LOCK_GUARD();
     /* Only check priorities when schedcfgen is set */
     schedcfgen = get_schedcfgen(env);
@@ -316,8 +324,7 @@ void hex_interrupt_update(CPUHexagonState *env)
     g_assert(bql_locked());
     if (get_ipend(env) != 0) {
         CPU_FOREACH(cs) {
-            HexagonCPU *hex_cpu = HEXAGON_CPU(cs);
-            CPUHexagonState *hex_env = &hex_cpu->env;
+            CPUHexagonState *hex_env = cpu_env(cs);
             const int exe_mode = get_exe_mode(hex_env);
             if (exe_mode != HEX_EXE_MODE_OFF) {
                 cs->interrupt_request |= CPU_INTERRUPT_SWI;
