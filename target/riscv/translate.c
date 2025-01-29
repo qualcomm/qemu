b0d4fe3321c51ba8f8b7846f9f1efff01767f13f
Anton Johansson <anjo@rev.ng>
2025-01-29 15:43:19 +0100

    target/riscv: Xqci, add instruction decoders
    Introduces the Xqci extension and instruction encodings.
    
    Signed-off-by: Anton Johansson <anjo@rev.ng>

--- target/riscv/translate.c
+++ target/riscv/translate.c
@@ -1302,6 +1302,19 @@ static void xqci_raise_IllegalInstruction(DisasContext *ctx)
     gen_helper_raise_exception(tcg_env, tcg_constant_tl(RISCV_EXCP_ILLEGAL_INST));
 }
 
+static uint64_t decode_xqci_48_load_bytes(DisasContext *ctx, uint64_t insn,
+                                          int offset, int length)
+{
+    return 0;
+}
+
+#include "decode-xqci-16.c.inc"
+#include "decode-xqci-32.c.inc"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-function"
+#include "decode-xqci-48.c.inc"
+#pragma GCC diagnostic pop
+
 #include "xqci/xqci-tcg.c"
 #include "xqci/xqci-tcg-manual.c.inc"
 #include "xqci/xqci-trans-decode.c.inc"
@@ -1312,15 +1325,24 @@ static void xqci_raise_IllegalInstruction(DisasContext *ctx)
 #define MAX_INSN_LEN  8
 
 const RISCVDecoder16 decoder_table_16[] = {
+#ifdef TARGET_RISCV32
+    { has_xqci_p, decode_xqci_16},
+#endif
 };
 
 const RISCVDecoder32 decoder_table_32[] = {
     { always_true_p, decode_insn32 },
     { has_xthead_p, decode_xthead},
     { has_XVentanaCondOps_p, decode_XVentanaCodeOps},
+#ifdef TARGET_RISCV32
+    { has_xqci_p, decode_xqci_32},
+#endif
 };
 
 const RISCVDecoder48 decoder_table_48[] = {
+#ifdef TARGET_RISCV32
+    { has_xqci_p, decode_xqci_48},
+#endif
 };
 
 const size_t decoder_table_size_16 = ARRAY_SIZE(decoder_table_16);
