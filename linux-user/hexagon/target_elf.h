f1354ae99cf8034915f2fde9091604788a4d4e00
Brian Cain <brian.cain@gmail.com>
2025-02-19 21:06:06 -0600

    target/hexagon: Add support for v79 executables
    Treat v79 like a v73 for now.
    
    No v79-specific opcodes are yet supported.
    
    Signed-off-by: Brian Cain <brian.cain@gmail.com>

--- linux-user/hexagon/target_elf.h
+++ linux-user/hexagon/target_elf.h
@@ -38,7 +38,8 @@ static inline const char *cpu_get_model(uint32_t eflags)
         eflags == 0x69 ||    /* v69 */
         eflags == 0x71 ||    /* v71 */
         eflags == 0x8071 ||  /* v71t */
-        eflags == 0x73       /* v73 */
+        eflags == 0x73 ||    /* v73 */
+        eflags == 0x79       /* v79 */
        ) {
         return "v73";
     }
