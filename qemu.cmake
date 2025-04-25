7ae42af2e211c518964cbe1414e5a4203bf76a15
Jerome Haxhiaj <jhaxhiaj@qti.qualcomm.com>
2025-04-25 09:16:49 +0200

    config: Enable vnc jpeg compression
    Signed-off-by: Jerome Haxhiaj <jhaxhiaj@qti.qualcomm.com>

--- qemu.cmake
+++ qemu.cmake
@@ -9,10 +9,10 @@ set(QEMU_CONF_ARGS
     --disable-sparse
     --enable-sdl
     --enable-vnc
+    --enable-vnc-jpeg
+    --disable-vnc-sasl
     --disable-xen
     --disable-brlapi
-    --disable-vnc-sasl
-    --disable-vnc-jpeg
     --disable-png
     --disable-curses
     --disable-curl
