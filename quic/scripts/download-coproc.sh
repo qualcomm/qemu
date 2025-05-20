4ce9fd4a92242cc8cc1778b125a47573a06caf1a
Matheus Tavares Bernardino <matheus.bernardino@oss.qualcomm.com>
2025-05-20 12:55:35 -0700

    CI: fix coproc reference
    We are now using a tag based system to reference the coproc. This fixes
    the v81 coproc tests.
    
    Signed-off-by: Matheus Tavares Bernardino <matheus.bernardino@oss.qualcomm.com>

--- quic/scripts/download-coproc.sh
+++ quic/scripts/download-coproc.sh
@@ -6,25 +6,16 @@ readonly JOB="${1}"
 readonly OUTPUT_DIR="${2}"
 readonly BASE_URL="${3}"
 readonly TOKEN="${4}"
+readonly REF_NAME="${5}"
 
 readonly ARCHIVE="coproc.zip"
 readonly ID=7631
+readonly URL="${BASE_URL}/api/v4/projects/${ID}/jobs/artifacts/${REF_NAME}/download?job=${JOB}"
 
 if [ -z "${JOB}" ] || [ -z "${BASE_URL}" ] || [ -z "${TOKEN}" ] || [ -z "${OUTPUT_DIR}" ]; then
     echo "Error: Missing argument"
     exit 1
 fi
-
-# Determine if the currently checked out commit matches a tag
-TAG="$(git describe --tags --exact-match --match qemu-hexagon-* 2>/dev/null || echo no)"
-REF_NAME="main"
-if test "${TAG}" != no
-then
-    REF_NAME="$(echo "qemu-coproc-plugin-${TAG}" | sed -e 's/qemu-hexagon-//')"
-fi
-
-readonly REF_NAME
-readonly URL="${BASE_URL}/api/v4/projects/${ID}/jobs/artifacts/${REF_NAME}/download?job=${JOB}"
 
 curl --location --output "${ARCHIVE}" --header "JOB-TOKEN: ${TOKEN}" "${URL}"
 unzip -d "${OUTPUT_DIR}" "${ARCHIVE}"
