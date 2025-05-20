8b4a7e1fd5a0bf7f91d80f64e1f284a812d18ea4
Marco Liebel <marco.liebel@oss.qualcomm.com>
2025-05-20 14:16:40 -0700

    Update checkpatch job
    Gitlab ci defines a couple of stages implicitly
    (https://docs.gitlab.com/ci/yaml/#stages). One of those stages is called
    `.pre`. It's the very first stage that runs.
    
    Checkpatch needs more than `GIT_DEPTH: 1` because it's trying to check
    all commits of an MR against the target branch.
    
    Signed-off-by: Marco Liebel <marco.liebel@oss.qualcomm.com>

--- quic-gitlab-ci.d/checkpatch.sh
+++ quic-gitlab-ci.d/checkpatch.sh
@@ -1,16 +1,22 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+# shellcheck source=/dev/null
+. "$(pwd)/quic-gitlab-ci.d/scripts/merge-request.sh"
+
+set +e
+set -x
 
 error=0
 
-for rev in $(git rev-list --first-parent $1..$2)
+for rev in $(git rev-list --first-parent "${TARGET_REF}".."${SOURCE_REF}")
 do
-    git rev-parse --verify -q $rev^2 >/dev/null && continue
-    echo "========= Checking commit $(git log --pretty=reference $rev^!)"
-    ./scripts/checkpatch.pl --color=always --no-signoff --branch "$rev^!"
-    error=$(($error || $?))
+    git rev-parse --verify -q "${rev}^2" >/dev/null && continue
+    echo "========= Checking commit $(git log --pretty=reference "${rev}^!")"
+    ./scripts/checkpatch.pl --color=always --no-signoff --branch "${rev}^!"
+    error=$((error || ${?}))
 done
 
-if test $? -ne 0 || test $error -ne 0
+if test "${?}" -ne 0 || test "${error}" -ne 0
 then
     exit 1
 fi
