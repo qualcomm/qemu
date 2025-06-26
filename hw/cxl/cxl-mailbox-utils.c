ad76a7d9f3af130cda12335dff684b809b54dec6
Anisa Su <anisa.su@samsung.com>
2025-06-26 23:41:15 +0000

    hw/cxl: Move command code enum + defines to cxl_mailbox.h
    Per the spec, FMAPI commands (0x51-0x59) must be bound with
    MCTP_MT_CXL_FMAPI. To make the neccessary enum visible for
    enforcing this in the MCTP emulation, move them to the header.
    
    Signed-off-by: Anisa Su <anisa.su@samsung.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- hw/cxl/cxl-mailbox-utils.c
+++ hw/cxl/cxl-mailbox-utils.c
@@ -39,7 +39,7 @@
 
 /*
  * How to add a new command, example. The command set FOO, with cmd BAR.
- *  1. Add the command set and cmd to the enum.
+ *  1. Add the command set and cmd to the enum in cxl_mailbox.h.
  *     FOO    = 0x7f,
  *          #define BAR 0
  *  2. Implement the handler
@@ -61,79 +61,6 @@
  *  XXX: The handler need not worry about endianness. The payload is read out of
  *  a register interface that already deals with it.
  */
-
-enum {
-    INFOSTAT    = 0x00,
-        #define IS_IDENTIFY   0x1
-        #define BACKGROUND_OPERATION_STATUS    0x2
-        #define GET_RESPONSE_MSG_LIMIT         0x3
-        #define SET_RESPONSE_MSG_LIMIT         0x4
-        #define BACKGROUND_OPERATION_ABORT     0x5
-    EVENTS      = 0x01,
-        #define GET_RECORDS   0x0
-        #define CLEAR_RECORDS   0x1
-        #define GET_INTERRUPT_POLICY   0x2
-        #define SET_INTERRUPT_POLICY   0x3
-    FIRMWARE_UPDATE = 0x02,
-        #define GET_INFO      0x0
-        #define TRANSFER      0x1
-        #define ACTIVATE      0x2
-    TIMESTAMP   = 0x03,
-        #define GET           0x0
-        #define SET           0x1
-    LOGS        = 0x04,
-        #define GET_SUPPORTED 0x0
-        #define GET_LOG       0x1
-        #define GET_LOG_CAPABILITIES   0x2
-        #define CLEAR_LOG     0x3
-        #define POPULATE_LOG  0x4
-    FEATURES    = 0x05,
-        #define GET_SUPPORTED 0x0
-        #define GET_FEATURE   0x1
-        #define SET_FEATURE   0x2
-    MAINTENANCE = 0x06,
-        #define PERFORM 0x0
-    IDENTIFY    = 0x40,
-        #define MEMORY_DEVICE 0x0
-    CCLS        = 0x41,
-        #define GET_PARTITION_INFO     0x0
-        #define GET_LSA       0x2
-        #define SET_LSA       0x3
-    HEALTH_INFO_ALERTS = 0x42,
-        #define GET_ALERT_CONFIG 0x1
-        #define SET_ALERT_CONFIG 0x2
-    SANITIZE    = 0x44,
-        #define OVERWRITE     0x0
-        #define SECURE_ERASE  0x1
-        #define MEDIA_OPERATIONS 0x2
-    PERSISTENT_MEM = 0x45,
-        #define GET_SECURITY_STATE     0x0
-    MEDIA_AND_POISON = 0x43,
-        #define GET_POISON_LIST        0x0
-        #define INJECT_POISON          0x1
-        #define CLEAR_POISON           0x2
-        #define GET_SCAN_MEDIA_CAPABILITIES 0x3
-        #define SCAN_MEDIA             0x4
-        #define GET_SCAN_MEDIA_RESULTS 0x5
-    DCD_CONFIG  = 0x48,
-        #define GET_DC_CONFIG          0x0
-        #define GET_DYN_CAP_EXT_LIST   0x1
-        #define ADD_DYN_CAP_RSP        0x2
-        #define RELEASE_DYN_CAP        0x3
-    PHYSICAL_SWITCH = 0x51,
-        #define IDENTIFY_SWITCH_DEVICE      0x0
-        #define GET_PHYSICAL_PORT_STATE     0x1
-        #define PHYSICAL_PORT_CONTROL       0x2
-    TUNNEL = 0x53,
-        #define MANAGEMENT_COMMAND     0x0
-    FMAPI_DCD_MGMT = 0x56,
-        #define GET_DCD_INFO    0x0
-        #define GET_HOST_DC_REGION_CONFIG   0x1
-        #define SET_DC_REGION_CONFIG        0x2
-        #define GET_DC_REGION_EXTENT_LIST   0x3
-        #define INITIATE_DC_ADD             0x4
-        #define INITIATE_DC_RELEASE         0x5
-};
 
 /* CCI Message Format CXL r3.1 Figure 7-19 */
 typedef struct CXLCCIMessage {
