93ace5def4b909a26055d6c73db2269aa7da5756
Matheus Tavares Bernardino <matheus.bernardino@oss.qualcomm.com>
2025-08-15 08:02:26 -0700

    CI: verif job: compare against latest nightly, not latest release
    Comparing against the latest release sounded like a good idea at first,
    but it introduces too much noise as bugfixes made on master but not yet
    cherry-picked to a release will cause the verif job to fail. Instead,
    let's compare against the latest nightly build, which should isolate
    bugs introduced by the current MR or the current nightly.
    
    A small adjustment is required, though: we take the penultimate nightly,
    not the latest one, as we could be running the latest one.
    
    Signed-off-by: Matheus Tavares Bernardino <matheus.bernardino@oss.qualcomm.com>

--- quic-gitlab-ci.d/scripts/packet-verif.sh
+++ quic-gitlab-ci.d/scripts/packet-verif.sh
@@ -29,43 +29,19 @@ ${script}")"
 
 git clone https://gitlab-ci-token:"${CI_JOB_TOKEN}"@"${VERIF_URL}"
 
-tag="$(git ls-remote --tags origin 'qemu-hexagon-[0-9]*' |\
-       sed -n 's/.*[\s\t]refs\/tags\/\([^}]*\)$/\1/p' | tail -n1)"
-
-if test -z "${tag}"
-then
-    echo "tag not found '${tag}'"
-    exit 1
-fi
-
-api "projects/${CI_PROJECT_ID}/pipelines?ref=${tag}&per_page=1" \
-    'print(data[0]["id"])'
+# Take the penultimate nightly (as we might be running the latest right now)
+api "projects/${CI_PROJECT_ID}/pipelines?ref=master&source=schedule&per_page=2" \
+    'print(data[1]["id"])'
 
 api "projects/${CI_PROJECT_ID}/pipelines/${out}/jobs?per_page=100" \
 'for d in data:
-    if d["name"] in ("hexagon_tarball_tag", "hexagon-linux-x86_64-tag"):
-        print(f"{d['\''name'\'']},{d['\''id'\'']}")
+    if d["name"] == "hexagon-linux-x86_64":
+        print(d["id"])
         break'
 
-jobname="$(echo "${out}" | cut -d, -f1)"
-jobid="$(echo "${out}" | cut -d, -f2)"
-base=""
-
-if test "$jobname" == hexagon_tarball_tag
-then
-    # Old format
-    wget --no-verbose \
-        "https://gitlab.qualcomm.com/qqvp/qemu/qemu/-/jobs/6070923/artifacts/raw/tarball-artifacts/${tag}.tgz"
-    mkdir base
-    tar xzf "${tag}.tgz" -C base
-    base="$(realpath base/Tools/QEMUHexagon/bin/qemu-system-hexagon)"
-else
-    # New format
-    wget --no-verbose \
-        "https://gitlab.qualcomm.com/qqvp/qemu/qemu/-/jobs/${jobid}/artifacts/download"
-    unzip -q download -d base
-    base="$(realpath base/build/qemu-system-hexagon)"
-fi
+wget --no-verbose \
+    "https://gitlab.qualcomm.com/qqvp/qemu/qemu/-/jobs/${out}/artifacts/download"
+unzip -q download -d base
 
 toolchain="$(dirname "$(command -v hexagon-clang)")"
 echo "using toolchain '${toolchain}'"
@@ -75,4 +51,5 @@ cd "${VERIF_REPO_NAME}"
     -n 100 -l 0 --exit-code \
     --iset "$(realpath ../iset.py)" \
     --toolchain-path "${toolchain}" \
-    -b "${base}" -q "$(realpath ../build/qemu-system-hexagon)"
+    -b "$(realpath ../base/build/qemu-system-hexagon)" \
+    -q "$(realpath ../build/qemu-system-hexagon)"
