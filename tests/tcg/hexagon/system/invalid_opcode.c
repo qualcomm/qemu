d377769269ff152a9d788be2d1927cc03e32495c
Brian Cain <brian.cain@oss.qualcomm.com>
2025-10-30 10:53:52 -0500

    tests/tcg: Remove some systests from tcg
    These tests are functional tests, exercising the entire system emulation
    environment.  The tcg tests are intended to exercise instruction translation.
    
    These tests are now maintained in the public https://github.com/qualcomm/qemu-hexagon-testing
    repo.  This gives us the capability of creating test cases for the community
    and since it's public, it doesn't depend on QEMU_TEST_ALLOW_UNTRUSTED_CODE.
    
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- tests/tcg/hexagon/system/invalid_opcode.c
+++
@@ -1,8 +0,0 @@
-/*
- *  Copyright(c) 2023-2025 Qualcomm Innovation Center, Inc. All Rights Reserved.
- *
- *  SPDX-License-Identifier: GPL-2.0-or-later
- */
-
-#include "invalid_opcode.h"
-INVALID_OPCODE_MAIN("Hexagon invalid opcode test", invalid_opcode, 1)
