7added421876daab3669f78d25d65ad120e8f904
Matheus Tavares Bernardino <matheus.bernardino@oss.qualcomm.com>
2025-12-16 10:49:06 -0800

    Hexagon: throw exception on invalid packet
    Instead of a GLib assertion, let's raise an Hexagon exception on an
    invalid packet. This will usually cause a core dump, so while we are at
    it, let's also add the ELR to the core dump info to help users debug
    which packet is invalid.
    
    Signed-off-by: Matheus Tavares Bernardino <matheus.bernardino@oss.qualcomm.com>

--- target/hexagon/decode.c
+++ target/hexagon/decode.c
@@ -503,7 +503,6 @@ decode_insns(DisasContext *ctx, Insn *insn, uint32_t encoding)
             insn->iclass = iclass_bits(encoding);
             return 1;
         }
-        g_assert_not_reached();
     } else {
         uint32_t iclass = get_duplex_iclass(encoding);
         unsigned int slot0_subinsn = get_slot0_subinsn(encoding);
@@ -524,8 +523,12 @@ decode_insns(DisasContext *ctx, Insn *insn, uint32_t encoding)
                 return 2;
             }
         }
-        g_assert_not_reached();
     }
+    /*
+     * invalid/unrecognized opcode; return 1 and let gen_insn() raise an
+     * exception when it sees this empty insn.
+     */
+    return 1;
 }
 
 static void decode_add_endloop_insn(Insn *insn, int loopnum)
