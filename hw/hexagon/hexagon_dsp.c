bd2f0ec20d43987852f6cd9f177edfe0621a48a5
Brian Cain <brian.cain@oss.qualcomm.com>
2026-01-03 12:48:49 -0600

    fixes
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- hw/hexagon/hexagon_dsp.c
+++ hw/hexagon/hexagon_dsp.c
@@ -115,8 +115,11 @@ static void hexagon_common_init(MachineState *machine, Rev_t rev,
     Error **errp = NULL;
 
     DeviceState *glob_regs_dev = qdev_new(TYPE_HEXAGON_GLOBALREG);
+    object_property_add_child(OBJECT(machine), "global-regs",
+                              OBJECT(glob_regs_dev));
     qdev_prop_set_uint64(glob_regs_dev, "config-table-addr", m_cfg->cfgbase);
     qdev_prop_set_uint32(glob_regs_dev, "qtimer-base-addr", m_cfg->qtmr_region);
+    qdev_prop_set_uint32(glob_regs_dev, "dsp-rev", rev);
     sysbus_realize_and_unref(SYS_BUS_DEVICE(glob_regs_dev), errp);
 
     /* Create TLB object */
@@ -140,16 +143,17 @@ static void hexagon_common_init(MachineState *machine, Rev_t rev,
          * explicitly enabled via start instruction.
          */
         qdev_prop_set_bit(DEVICE(cpu), "start-powered-off", (i != 0));
+        qdev_prop_set_uint32(DEVICE(cpu), "dsp-rev", rev);
+        qdev_prop_set_uint32(DEVICE(cpu), "hvx-contexts",
+                             m_cfg->cfgtable.ext_contexts);
+        qdev_prop_set_uint32(DEVICE(cpu), "jtlb-entries",
+                             m_cfg->cfgtable.jtlb_size_entries);
         qdev_prop_set_uint32(DEVICE(cpu), "l2vic-base-addr", m_cfg->l2vic_base);
         if (!object_property_set_link(OBJECT(cpu), "global-regs",
                                       OBJECT(glob_regs_dev), errp)) {
             error_report("Failed to link global system registers to CPU %d", i);
             return;
         }
-        qdev_prop_set_uint32(DEVICE(cpu), "hvx-contexts",
-                             m_cfg->cfgtable.ext_contexts);
-        qdev_prop_set_uint32(DEVICE(cpu), "jtlb-entries",
-                             m_cfg->cfgtable.jtlb_size_entries);
 
 
         if (i == 0) {
@@ -169,7 +173,6 @@ static void hexagon_common_init(MachineState *machine, Rev_t rev,
         }
 
     }
-
     DeviceState *l2vic_dev = qdev_new(TYPE_L2VIC);
     sysbus_realize_and_unref(SYS_BUS_DEVICE(l2vic_dev), &error_fatal);
 
