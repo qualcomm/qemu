890201fa97f92a7ba78b791f20fa48616a104224
Brian Cain <brian.cain@oss.qualcomm.com>
2026-01-08 19:17:28 -0600

    hw/hexagon: Add smem initialization
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- hw/hexagon/hexagon_dsp.c
+++ hw/hexagon/hexagon_dsp.c
@@ -609,6 +609,8 @@ static void v73m_config_init(MachineState *machine)
     hexagon_common_init(machine, v73m_rev, &v73m);
 }
 
+#include "smem_entries.inc"
+
 static void SA8775P_cdsp0_config_init(MachineState *machine)
 {
     hexagon_common_init(machine, v73_rev, &SA8775P_cdsp0);
@@ -636,6 +638,8 @@ static void SA8775P_cdsp0_config_init(MachineState *machine)
     hwaddr cmd_db_header_addr = 0x0C3F0000;
     hwaddr cmd_db_bin_addr = 0x80860000;
     hexagon_load_cmd_db(cmd_db_header_addr, cmd_db_bin_addr);
+
+    cpu_physical_memory_write(0x90900000, sa8775p_smem_data, sizeof(sa8775p_smem_data));
 }
 
 static void SA8775P_cdsp0_init(ObjectClass *oc, const void *data)
