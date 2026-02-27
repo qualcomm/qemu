a1d18c0508f6f6d1c0bb5127a408fdc9a79080a1
Brian Cain <brian.cain@oss.qualcomm.com>
2026-02-26 19:39:41 -0800

    target/hexagon: guard semihosting against unrecognized SWI numbers
    ARM-compat semihosting SWI numbers are all <= 0x31. When guest code
    executes trap0(#0) with an arbitrary R0 value (e.g. during
    direct-to-guest interrupt handling), log the unrecognized request
    and return rather than forwarding to do_common_semihosting() which
    would abort.
    
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- target/hexagon/hexswi.c
+++ target/hexagon/hexswi.c
@@ -148,6 +148,19 @@ static void sim_handle_trap0(CPUHexagonState *env)
              */
             do_preload(env, swi_info, (what_swi == HEX_SYS_WRITE));
         }
+        /*
+         * ARM-compat semihosting SWI numbers are all <= 0x31.
+         * If R0 holds a value outside that range (e.g. guest code
+         * executing trap0(#0) with an arbitrary R0), treat it as an
+         * unrecognized request rather than forwarding to
+         * do_common_semihosting() which would abort.
+         */
+        if (what_swi > 0x31) {
+            qemu_log_mask(LOG_UNIMP,
+                          "trap0(#0): unrecognized request in r0: "
+                          "0x%x\n", what_swi);
+            return;
+        }
         CPUState *cs = env_cpu(env);
         do_common_semihosting(cs);
         return;
