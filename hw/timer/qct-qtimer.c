a5e7d131a292b85318a24b1d17f086867608db80
Brian Cain <brian.cain@oss.qualcomm.com>
2026-04-05 09:27:01 -0700

    hw/hexagon: use DEVICE_LITTLE_ENDIAN for L2VIC and QTimer
    Hexagon is a little-endian architecture. DEVICE_NATIVE_ENDIAN uses the
    host's byte order, which is incorrect for device MMIO regions that
    should always use the guest's byte order. Change all four sites in L2VIC
    and QTimer to use DEVICE_LITTLE_ENDIAN.
    
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- hw/timer/qct-qtimer.c
+++ hw/timer/qct-qtimer.c
@@ -121,7 +121,7 @@ static void qct_qtimer_write(void *opaque, hwaddr offset, uint64_t value,
 static const MemoryRegionOps qct_qtimer_ops = {
     .read = qct_qtimer_read,
     .write = qct_qtimer_write,
-    .endianness = DEVICE_NATIVE_ENDIAN,
+    .endianness = DEVICE_LITTLE_ENDIAN,
 };
 
 static const VMStateDescription vmstate_qct_qtimer = {
@@ -438,7 +438,7 @@ static MemTxResult hex_timer_write(void *opaque, hwaddr offset, uint64_t value,
 static const MemoryRegionOps hex_timer_ops = {
     .read_with_attrs = hex_timer_read,
     .write_with_attrs = hex_timer_write,
-    .endianness = DEVICE_NATIVE_ENDIAN,
+    .endianness = DEVICE_LITTLE_ENDIAN,
 };
 
 static const VMStateDescription vmstate_hex_timer = {
