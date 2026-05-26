9f0e2ae0c79360e9783af05cdfe1cce4bf550b33
Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>
2026-05-26 18:49:36 +0000

    [DO NOT MERGE] single-binary: compare command lines
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>

---
+++ cmdline_update.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+binaries()
+{
+    ls configs/targets/ |
+    grep -v meson.build |
+    grep -v bsd-user |
+    sed -e 's/\..*//' \
+        -e 's/\(.*\)-linux-user/qemu-\1/' \
+        -e 's/\(.*\)-softmmu/qemu-system-\1/'
+}
+
+filter_version()
+{
+    grep -v 'QEMU emulator version' || true
+}
+
+filter_usage()
+{
+    grep -v '^usage:' | grep -v '^-target' || true
+}
+
+filter_trailing_whitespace()
+{
+    sed -e 's/\s*$//'
+}
+
+f()
+{
+    filter_version | filter_usage | filter_trailing_whitespace
+}
+
+./build.sh all-debug
+
+ref=cmdline_ref
+rm -rf $ref
+mkdir -p $ref
+for bin in $(binaries); do
+    echo "$bin"
+    ./build/$bin -help |& f         > $ref/$bin.help
+    (./build/$bin -cpu help |& f || true) > $ref/$bin.cpu_help
+    if echo $bin | grep -q qemu-system; then
+        ./build/$bin -machine help |& f > $ref/$bin.machine_help
+        ./build/$bin -device help |& f  > $ref/$bin.device_help
+    fi
+done
