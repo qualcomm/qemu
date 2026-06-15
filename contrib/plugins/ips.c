c059928aaf3f23df9cb0f63c7d5a07a99c8b97f9
Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>
2026-06-15 12:35:07 -0700

    plugins: add userdata to qemu_plugin_register_vcpu_exit_cb
    Reviewed-by: Manos Pitsidianakis <manos.pitsidianakis@linaro.org>
    Link: https://lore.kernel.org/qemu-devel/20260615193526.2883349-9-pierrick.bouvier@oss.qualcomm.com
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>

--- contrib/plugins/ips.c
+++ contrib/plugins/ips.c
@@ -132,7 +132,7 @@ static void vcpu_resume(qemu_plugin_id_t id, unsigned int cpu_index)
     vcpu->sleeping = false;
 }
 
-static void vcpu_exit(unsigned int cpu_index)
+static void vcpu_exit(unsigned int cpu_index, void *userdata)
 {
     vCPUTime *vcpu = qemu_plugin_scoreboard_find(vcpus, cpu_index);
     update_system_time(vcpu);
@@ -254,7 +254,7 @@ QEMU_PLUGIN_EXPORT int qemu_plugin_install(qemu_plugin_id_t id,
 
     qemu_plugin_register_vcpu_tb_trans_cb(id, vcpu_tb_trans);
     qemu_plugin_register_vcpu_init_cb(id, vcpu_init, NULL);
-    qemu_plugin_register_vcpu_exit_cb(id, vcpu_exit);
+    qemu_plugin_register_vcpu_exit_cb(id, vcpu_exit, NULL);
     qemu_plugin_register_atexit_cb(id, plugin_exit, NULL);
 
     return 0;
