f89114e4cc75bf02058e8c9cb82233ef5cdd5384
Brian Cain <brian.cain@oss.qualcomm.com>
2026-06-18 20:15:19 -0700

    fixup! target/hexagon: Add new macro definitions for sysemu

--- target/hexagon/sys_macros.h
+++ target/hexagon/sys_macros.h
@@ -13,6 +13,8 @@
 
 #ifndef CONFIG_USER_ONLY
 
+#include "system/physmem.h"
+
 #ifdef QEMU_GENERATE
 #define GET_SSR_FIELD(RES, FIELD) \
     GET_FIELD(RES, FIELD, hex_t_sreg[HEX_SREG_SSR])
@@ -80,7 +82,7 @@
   const uintptr_t rs = ((unsigned long)(unsigned)(SRC1)) & 0x7ff; \
   const uintptr_t rt = ((unsigned long)(unsigned)(SRC2)) << 11; \
   const uintptr_t addr = rs + rt;         \
-  cpu_physical_memory_read(addr, &DST, sizeof(uint32_t)); \
+  physical_memory_read(addr, &DST, sizeof(uint32_t)); \
 }
 
 #define fPOW2_HELP_ROUNDUP(VAL) \
