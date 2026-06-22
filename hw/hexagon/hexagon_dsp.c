17dd8f8280680c5895c8417c7360d722be3383b6
Marco Liebel <marco.liebel@oss.qualcomm.com>
2026-06-22 14:21:41 -0700

    hw/hexagon: initialize boot_evb to silence GCC 15 warning
    GCC 15 warns about potentially uninitialized use of boot_evb.
    Zero-initialize it at declaration to suppress the warning.

--- hw/hexagon/hexagon_dsp.c
+++ hw/hexagon/hexagon_dsp.c
@@ -440,7 +440,7 @@ static void hexagon_common_init(MachineState *machine,
     sysbus_realize_and_unref(SYS_BUS_DEVICE(tlb_dev), errp);
 
     HexagonVersion hex_ver = machine_cpu_version(machine);
-    uint64_t boot_evb;
+    uint64_t boot_evb = 0;
     hexagon_init_bootstrap(dms, &hex_ver, &boot_evb);
     g_assert(hex_ver != HEX_VER_ANY && hex_ver != HEX_VER_NONE);
     qdev_prop_set_uint32(glob_regs_dev, "boot-evb", boot_evb);
