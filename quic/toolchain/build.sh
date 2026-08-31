9c885889dadd2ef80d2cee76bde470762b2e5e48
Marco Liebel <marco.liebel@oss.qualcomm.com>
2026-08-31 16:17:50 +0000

    quic/toolchain: update toolchain build script
    Use `versions.sh`

--- quic/toolchain/build.sh
+++ quic/toolchain/build.sh
@@ -1,42 +1,29 @@
 #!/usr/bin/env sh
+
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-set -eu
+set -eux
 
-SCRIPT_DIR="$(cd "$(dirname "${0}")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "${0}")" && pwd -P)"
 readonly SCRIPT_DIR
 
-if [ ${#} -lt 1 ]; then
-    printf "Usage: %s <revision> [<baremetal-ver>] [<musl-ver>]\n" "${0}" >&2
-    exit 1
-fi
+. "${SCRIPT_DIR}/../util/help.sh"
 
-HEXAGON_TOOLCHAIN_REVISION="${1}"
-readonly HEXAGON_TOOLCHAIN_REVISION
-BAREMETAL_VER="${2:-21.0.03}"
-readonly BAREMETAL_VER
-MUSL_VER="${3:-22.1.4}"
-readonly MUSL_VER
+readonly HELP_MESSAGE="Usage: ${0}
+
+Create and upload a Hexagon toolchain package."
+
+[ -z "${GITLAB_TOOLCHAIN_UPLOAD_TOKEN}" ] \
+    && print_help_error "GITLAB_TOOLCHAIN_UPLOAD_TOKEN missing"
 
 readonly WORKDIR="/tmp/hexagon-toolchain-build"
-
-if [ -z "${GITLAB_TOOLCHAIN_UPLOAD_TOKEN:-}" ]; then
-    printf "Error: GITLAB_TOOLCHAIN_UPLOAD_TOKEN is not set.\n" >&2
-    exit 1
-fi
 
 rm -rf "${WORKDIR}"
 mkdir -p "${WORKDIR}"
 cd "${WORKDIR}"
 
-"${SCRIPT_DIR}/fetch-hexagon-toolchains.sh" "${BAREMETAL_VER}" "${MUSL_VER}"
-
-"${SCRIPT_DIR}/add-linux-musl-support.sh" \
-    "./${BAREMETAL_VER}/Tools" \
-    "./clang+llvm-${MUSL_VER}-cross-hexagon-unknown-linux-musl/x86_64-linux-gnu"
-
-"${SCRIPT_DIR}/pack-hexagon-toolchain.sh" "./${BAREMETAL_VER}"
-
-"${SCRIPT_DIR}/upload-hexagon-toolchain.sh" \
-    "./hexagon-toolchain-${BAREMETAL_VER}-extended.tar.zst" \
-    "${HEXAGON_TOOLCHAIN_REVISION}"
+"${SCRIPT_DIR}/fetch-hexagon-toolchains.sh"
+"${SCRIPT_DIR}/add-linux-musl-support.sh"
+"${SCRIPT_DIR}/pack-hexagon-toolchain.sh"
+"${SCRIPT_DIR}/upload-hexagon-toolchain.sh"
