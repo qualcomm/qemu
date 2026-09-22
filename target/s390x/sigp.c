e6936b5c0ea72109d2ad3c42794924582ae70c55
Philippe Mathieu-Daudé <philmd@oss.qualcomm.com>
2026-09-22 10:36:38 +0200

    target/s390x: Use S390CpuState for CPU state APIs
    Use the QAPI S390CpuState enum for CPU state accessors instead
    of uint8_t. Handle state checks with switch statements and assert
    unexpected states in SIGP restart and stop handling.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@oss.qualcomm.com>
    ---
    FIXME: g_assert_not_reached -> break?

--- target/s390x/sigp.c
+++ target/s390x/sigp.c
@@ -39,7 +39,7 @@ static void set_sigp_status(SigpInfo *si, uint64_t status)
 
 static void sigp_sense(S390CPU *dst_cpu, SigpInfo *si)
 {
-    uint8_t state = s390_cpu_get_state(dst_cpu);
+    S390CpuState state = s390_cpu_get_state(dst_cpu);
     bool ext_call = dst_cpu->env.pending_int & INTERRUPT_EXTERNAL_CALL;
     uint64_t status = 0;
 
@@ -221,6 +221,8 @@ static void sigp_stop_and_store_status(CPUState *cs, run_on_cpu_data arg)
         cpu_synchronize_state(cs);
         s390_store_status(cpu, S390_STORE_STATUS_DEF_ADDR, true);
         break;
+    default:
+        g_assert_not_reached();
     }
     si->cc = SIGP_CC_ORDER_CODE_ACCEPTED;
 }
@@ -362,6 +364,8 @@ static void sigp_restart(CPUState *cs, run_on_cpu_data arg)
     case S390_CPU_STATE_OPERATING:
         cpu_inject_restart(cpu);
         break;
+    default:
+        g_assert_not_reached();
     }
     si->cc = SIGP_CC_ORDER_CODE_ACCEPTED;
 }
