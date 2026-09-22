005605967690af1cf1f8d594830dc2c54c03e038
Philippe Mathieu-Daudé <philmd@oss.qualcomm.com>
2026-09-22 08:54:39 +0200

    target/arm: Access PSCI state atomically
    Use qatomic_set() when updating ARMCPU::power_state and
    CPUARMState::halt_reason. Read ARMCPU::power_state atomically in
    arm_cpu_has_work(), which can run without the BQL held.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@oss.qualcomm.com>

--- target/arm/arm-powerctl.c
+++ target/arm/arm-powerctl.c
@@ -50,8 +50,9 @@ void arm_set_cpu_power_state(ARMCPU *cpu, ARMPSCIState state)
     CPUARMState *env = &cpu->env;
     CPUState *cs = CPU(cpu);
 
-    cpu->power_state = state;
-    env->halt_reason = state == PSCI_OFF ? HALT_PSCI : NOT_HALTED;
+    qatomic_set(&cpu->power_state, state);
+    qatomic_set(&env->halt_reason, state == PSCI_OFF ? HALT_PSCI
+                                                     : NOT_HALTED);
 
     if (!qemu_cpu_is_self(cs)) {
         qemu_cpu_kick(cs);
