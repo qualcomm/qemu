3d386c040685bf7bb5df24992134c7f8f4a059e5
Philippe Mathieu-Daudé <philmd@oss.qualcomm.com>
2026-09-22 08:39:56 +0200

    target/arm: Kick vCPU after updating PSCI power state
    PSCI can update the power state of a vCPU other than the caller.
    Kick that vCPU after changing its state so it observes the update
    promptly.
    
    Reported-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@oss.qualcomm.com>

--- target/arm/arm-powerctl.c
+++ target/arm/arm-powerctl.c
@@ -48,9 +48,14 @@ struct CpuOnInfo {
 void arm_set_cpu_power_state(ARMCPU *cpu, ARMPSCIState state)
 {
     CPUARMState *env = &cpu->env;
+    CPUState *cs = CPU(cpu);
 
     cpu->power_state = state;
     env->halt_reason = state == PSCI_OFF ? HALT_PSCI : NOT_HALTED;
+
+    if (!qemu_cpu_is_self(cs)) {
+        qemu_cpu_kick(cs);
+    }
 }
 
 static void arm_set_cpu_on_async_work(CPUState *target_cpu_state,
